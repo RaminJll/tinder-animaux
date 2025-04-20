42d76fec6a9da237b17dfadf36e39209019ba240
RaminJll <150394222+RaminJll@users.noreply.github.com>
2025-04-20 21:45:00 +0200

    Update authMiddleware.js

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -4,20 +4,20 @@ const jwtSecret = process.env.JWT_SECRET;
 const Organization = require('./models/Organization');
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1]; // Récupérer le token de l'en-tête
+    const token = req.header('Authorization')?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Accès non autorisé' });
     }
 
     try {
-        const decoded = jwt.verify(token, jwtSecret); // Remplacez par votre secret JWT
-        const organization = await Organization.findById(decoded.id); // Assurez-vous que le payload contient l'ID
+        const decoded = jwt.verify(token, jwtSecret); 
+        const organization = await Organization.findById(decoded.id);
         if (!organization) {
             return res.status(404).json({ message: 'Organisation introuvable' });
         }
 
-        req.user = organization; // Ajouter l'organisation au req
+        req.user = organization; 
         next();
     } catch (error) {
         res.status(401).json({ message: 'Token invalide' });
